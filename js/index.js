7c5468a1cad7efd056c001109d633dab0b192f9a
Ravali <ravali9sri@gmail.com>
2021-01-23 08:04:11 -0600

    Adding all files

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $(".owl-carousel").owlCarousel({
+    
+    autoPlay: 3000,
+    items : 5,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [979,3],
+    center: true,
+    nav:true,
+    loop:true,
+    responsive: {
+    600: {
+    items: 5
+    }
+    }      
+    
+    });
+    
+    });
