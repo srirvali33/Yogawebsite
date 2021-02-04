682a04c5f3c0e8321b90c604b237d4aeafdde784
Ravali <ravali9sri@gmail.com>
2021-02-03 22:42:42 -0600

    commit

--- js/index.js
+++ js/index.js
@@ -1,20 +1,74 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $(".owl-carousel").owlCarousel({
-    
-    autoPlay: 3000,
-    items : 5,
-    itemsDesktop : [1199,3],
-    itemsDesktopSmall : [979,3],
-    center: true,
-    nav:true,
-    loop:true,
-    responsive: {
-    600: {
-    items: 5
+ 
+    $(window).scroll(function () {
+      
+      $(".home-about-area .about-images .addimage").fadeIn();
+        
+      $('.home-about-area .about-images .addimage').css('display', 'flex');
+        
+       
+      });
+
+  /*-------------------------------
+ Counter Up
+ ---------------------------------*/
+ 
+
+ $(".counter").counterUp({
+  delay: 20,
+  time: 4000,
+  
+});
+
+  
+
+
+  /*------------------
+    Back to top
+  --------------------*/
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 500) {
+      $('.back-to-top').fadeIn();
+      $('.back-to-top').css('display', 'flex');
+    } else {
+      $('.back-to-top').fadeOut();
     }
-    }      
-    
-    });
-    
-    });
+  });
+
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+  });
+
+
+
+
+  /*-------------------------------
+owl carousel
+---------------------------------*/
+
+  $(".owl-carousel").owlCarousel({
+
+    autoPlay: 3000,
+    items: 5,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [979, 3],
+    center: true,
+    nav: true,
+    loop: true,
+    responsive: {
+      600: {
+        items: 5
+      }
+    }
+
+
+
+
+  });
+
+
+
+
+});
+
